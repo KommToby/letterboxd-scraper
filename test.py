1c6a151ccfe1eb1626f3e7c70d3715055d649871
Toby <tobycsmith18@gmail.com>
2024-06-12 14:56:02 +0100

    Updated to cmitchell version

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ Test account:
 '''
 
 
-
-import requests
 import sys
 import re
+
+import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import json
@@ -23,20 +23,50 @@ from time import sleep, time
 # Don't bother constructing console objects in every function.
 
 import argparse
+
+from nltk.corpus.reader import reviews
+
 parser = argparse.ArgumentParser(description='letterboxd args')
 parser.add_argument('--user', '-u', dest='user', help='letterboxd.com user')
 parser.add_argument('--reviews', '-r', dest='reviews', action="store_true", default=False, help='Gets reviews')
-parser.add_argument('--testing', '-t', dest='testing', action='store_true', default=False, help='Testing flag - for development only')
-parser.add_argument('--save-json', '-j', dest='json', action="store_true", default=False, help='Saves a JSON file of the reviews dictionary')
-parser.add_argument('--save-html', '-w', dest='html', action="store_true", default=False, help='Saves an HTML document for easily viewing reviews')
-parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_true", default=False, help='Opens saved HTML document in the browser')
+parser.add_argument('--testing', '-t', dest='testing', action='store_true', default=False,
+                    help='Testing flag - for development only')
+parser.add_argument('--save-json', '-j', dest='json', action="store_true", default=False,
+                    help='Saves a JSON file of the reviews dictionary')
+parser.add_argument('--save-html', '-w', dest='html', action="store_true", default=False,
+                    help='Saves an HTML document for easily viewing reviews')
+parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_true", default=False,
+                    help='Opens saved HTML document in the browser')
 parser.add_argument('--search', '-s', nargs='+', dest='search', default=())
 args = parser.parse_args()
 
-
 from rich.console import Console
 from rich import print as rprint
+
 console = Console()
+args.user = "kfavs"
+args.reviews = True
+args.html = False
+args.json = True
+
+
+#How many reviews do i have?
+"""
+import os
+directory = "C:\\Users\Connor\Desktop\letterboxd\letterboxdCorpus"
+total = 0
+for file in os.listdir(directory):
+    if file.endswith('.json'):
+        file_path = os.path.join(directory, file)
+        with open(file_path, 'r') as file1:
+            data = file1.readlines()
+            # Counting lines in JSON file
+            line_count = len(data)
+            total += line_count
+            total -= 5
+rprint("total reviews: ", total)
+"""
+
 # =======================================================================================================================================================================
 
 # Make a list of the pages of reviews. (Ex. /user/films/reviews/page/1 ... /user/films/reviews/page/5)
@@ -60,7 +90,7 @@ def getReviewUrls(user):
 
 
 def getSingleReview(url=''):
-    for possibleUrl in (url, url + '/1/'): # super-8 review had a an extra /1/ on the end
+    for possibleUrl in (url, url + '/1/'):  # super-8 review had a an extra /1/ on the end
         soup = BeautifulSoup(requests.get(possibleUrl).text, 'html.parser')
         reviewDivHtmlStr = str(soup.find("div", {'class': "review body-text -prose -hero -loose"}))
         sleep(.05)
@@ -69,37 +99,35 @@ def getSingleReview(url=''):
             my_string = re.sub(r'<(\/?p|br).*?>', ' ', my_string)
             return clean(my_string)
 
-# Stolen this from /u/cmitchellstudent
-# Trying to build a version of this which is still usable but works with correct parameter parsing
 
+# Should probably make different functions for batch getting all movies and searching.
+# Because args.search should only be checked once, not every time there's another movie review.
+# But it was too easy to just throw that in and add a continue
 def clean(string):
     string = string.replace('\n', ' ').replace('\\', ' ').replace('’', '\'')
     string = string.replace('“', '\"').replace('”', '\"').replace("…", "...")
     string = string.replace('This review may contain spoilers. I can handle the truth.', ' ')
     return string
-
-# Should probably make different functions for batch getting all movies and searching.
-# Because args.search should only be checked once, not every time there's another movie review.
-# But it was too easy to just throw that in and add a continue
-
 def getReviews(user):
     movieDelim = f'[red]{"=" * 80}'
     look = f'/{user}/film/'
     reviewsText = {}
 
-    # if args.search:
-    #     console.print(movieDelim)
-    #     for url in [f'https://letterboxd.com/{user}/film/{movie}/' for movie in args.search]:
-    #         movie = url.split('/film/')[-1][:-1]
-    #         console.print(f'[cyan]movie: [bold blue]{movie}')
-    #         console.print(f'\t[green]Searching')
-    #         console.print(f'\t[green]Requesting: {url}')
-    #         console.print(movieDelim)
 
-    #         reviewsText[movie] = getSingleReview(url=url)
-    #     return reviewsText
+    #not using search
+    """
+    if args.search:
+        console.print(movieDelim)
+        for url in [f'https://letterboxd.com/{user}/film/{movie}/' for movie in args.search]:
+            movie = url.split('/film/')[-1][:-1]
+            console.print(f'[cyan]movie: [bold blue]{movie}')
+            console.print(f'\t[green]Searching')
+            console.print(f'\t[green]Requesting: {url}')
+            console.print(movieDelim)
 
-
+            reviewsText[movie] = getSingleReview(url=url)
+        return reviewsText
+    """
     reviewUrls = getReviewUrls(user)
     console.print('[cyan] Urls with multiple reviews')
     rprint(reviewUrls)
@@ -118,7 +146,7 @@ def getReviews(user):
         valuableEnd = htmlText.find('</section>', valuableStart)
         smallStr = htmlText[valuableStart:valuableEnd]
 
-        # Again stolen from /u/cmitchellstudent
+        #MOST OF MY CODE===============================================================================================
 
         titles = []
         reviews = []
@@ -155,6 +183,41 @@ def getReviews(user):
             else:
                 reviewsText[titles[i]] = reviews[i]
 
+
+
+        """
+        #doesnt run this loop for some reason
+        for line in [line for line in smallStr.splitlines() if line.strip()[10:24] == '"film-detail">']:
+
+            shorterLine = line[line.find('<p>'):]
+
+            movieSearch = re.search(fr'{look}.*?/', shorterLine).group()
+            movie = re.sub(fr'{look}', '', str(movieSearch))[:-1]
+
+
+            console.print(f'[cyan]movie: [bold blue]{movie}')
+            reviewPreview = shorterLine[:shorterLine.find('</div>')].strip()
+
+            if '…' == reviewPreview[-5]:  # NOT THREE PERIODS - DIFFERENT UNICODE CHAR
+                movieReviewUrl = f'https://letterboxd.com/{user}/film/{movie}/'
+                console.print('\t[magenta]Preview contains partial review')
+                console.print(f'\t[magenta]Requesting: {movieReviewUrl}')
+                console.print(movieDelim)
+
+                reviewsText[movie] = getSingleReview(url=movieReviewUrl)
+                console.print(reviewsText)
+
+            else:
+                console.print('\t[blue]Preview contains full review')
+                console.print('\t[blue]No need to request individual page')
+                console.print(movieDelim)
+
+                reviewsText[movie] = reviewPreview
+                console.print(reviewsText)
+
+            sleep(.05)
+            """
+    #rprint(reviewsText)
     return reviewsText
 
 
@@ -167,7 +230,7 @@ def writeReviews(reviewsDict={}):
         fname = f'{user}_searched_reviews.html'
     rprint(f'html={fname}')
 
-    with open(fname, 'w+') as f:
+    with open(fname, 'w+', encoding="utf-16") as f:
         f.write('<!DOCTYPE html>\n')
         f.write('<html>\n')
         f.write('<head>\n')
@@ -196,8 +259,9 @@ def letterboxdRun():
     if args.reviews:
         fname = f'{user}_reviews.json'
         reviewsText = getReviews(user)
-
+        console.print(reviewsText)
         outputDict = {'user': user, 'reviews': reviewsText}
+
 
         if args.html:
             writeReviews(outputDict)
@@ -211,9 +275,10 @@ def letterboxdRun():
 
 if __name__ == '__main__':
     console.print('[cyan]*Command line arguments* ')
-    for k,v in vars(args).items():
+    for k, v in vars(args).items():
         rprint(f'\t{k}={v}')
     print()
 
-    console.print('[cyan]--Making requests to letterboxd.com--\n[red]This may take some time depending on how many reviews there are.\n')
+    console.print(
+        '[cyan]--Making requests to letterboxd.com--\n[red]This may take some time depending on how many reviews there are.\n')
     letterboxdRun()
